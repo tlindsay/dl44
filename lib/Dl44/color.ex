630e3cc6494a8519638b5bb6c084a7e121547d0f
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-12-11 12:23:31 -0600

    Implement Color module

---
+++ lib/Dl44/color.ex
@@ -0,0 +1,41 @@
+defmodule Dl44.Color do
+  alias Dl44.Color
+  defstruct r: 0.0, g: 0.0, b: 0.0
+
+  defguard is_numeric(val) when is_integer(val) or is_float(val)
+
+  @type color ::%__MODULE__{
+          r: float,
+          g: float,
+          b: float
+        }
+
+  def new(n \\ 0.0) when is_numeric(n), do: %Color{r: n/1, g: n/1, b: n/1}
+  def new(%{r: r, g: g, b: b}), do: %Color{r: r/1, g: g/1, b: b/1}
+  def new([r, g, b]), do: %Color{r: r/1, g: g/1, b: b/1}
+  def new(r, g, b), do: %Color{r: r/1, g: g/1, b: b/1}
+
+  def add(%Color{r: r1, g: g1, b: b1}, %Color{r: r2, g: g2, b: b2}) do
+    Color.new(
+      r1 + r2,
+      g1 + g2,
+      b1 + b2
+    )
+  end
+
+  def sub(%Color{r: r1, g: g1, b: b1}, %Color{r: r2, g: g2, b: b2}) do
+    Color.new(
+      r1 - r2,
+      g1 - g2,
+      b1 - b2
+    )
+  end
+
+  def scale(%Color{r: r, g: g, b: b}, n) when is_numeric(n) do
+    Color.new(
+      r * n,
+      g * n,
+      b * n
+    )
+  end
+end
---
+++ test/Dl44/color_test.exs
@@ -0,0 +1,52 @@
+defmodule Dl44.ColorTest do
+  use ExUnit.Case
+  doctest Dl44.Color
+
+  alias Dl44.Color
+
+  describe "Color.new" do
+    test "accepts a map" do
+      assert Color.new(%{r: 1, g: 2, b: 3}) == %Color{r: 1.0, g: 2.0, b: 3.0}
+    end
+
+    test "accepts a single value" do
+      assert Color.new(1) == %Color{r: 1.0, g: 1.0, b: 1.0}
+    end
+
+    test "accepts a list" do
+      assert Color.new([1, 2, 3]) == %Color{r: 1.0, g: 2.0, b: 3.0}
+    end
+
+    test "accepts no arguments" do
+      assert Color.new() == %Color{r: 0.0, g: 0.0, b: 0.0}
+    end
+
+    test "accepts a list of arguments" do
+      assert Color.new(1, 2, 3) == %Color{r: 1.0, g: 2.0, b: 3.0}
+    end
+  end
+
+  describe "Color arithmetc" do
+    test "Color.add" do
+      c1 = Color.new(1.0, 0.0, 0.0)
+      c2 = Color.new(0.0, 1.0, 0.0)
+
+      assert Color.add(c1, c2) == %Color{r: 1.0, g: 1.0, b: 0.0}
+    end
+
+    test "Color.sub" do
+      c1 = Color.new(1.0)
+      c2 = Color.new(0.2)
+
+      assert Color.sub(c1, c2) == %Color{r: 0.8, g: 0.8, b: 0.8}
+    end
+
+    test "Color.scale" do
+      c = Color.new(0.5)
+
+      assert Color.scale(c, 2.0) == %Color{r: 1.0, g: 1.0, b: 1.0}
+    end
+  end
+end
+
+
