db4c75dc5a0bcf049c4f537768df15c386de9f78
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-12-11 16:50:39 -0600

    Implement PPM image output

--- lib/Dl44/canvas.ex
+++ lib/Dl44/canvas.ex
@@ -5,7 +5,7 @@ defmodule Dl44.Canvas do
   defstruct width: 0, height: 0, pixels: Matrix
 
   def new(width \\ 0, height \\ 0, color \\ Color.new) do
-    pixels = Matrix.new(width, height, color)
+    pixels = Matrix.new(height, width, color)
     %Canvas{width: width, height: height, pixels: pixels}
   end
 
@@ -13,11 +13,42 @@ defmodule Dl44.Canvas do
     Matrix.elem(p, x, y)
   end
 
-  def write_pixel(%Canvas{width: w, height: h, pixels: p} = canvas, x, y, %Color{} = color) when is_integer(x) and is_integer(y) do
+  def write_pixel(%Canvas{width: w, height: h, pixels: p}, x, y, %Color{} = color) when is_integer(x) and is_integer(y) do
     %Canvas{
       width: w,
       height: h,
-      pixels: Matrix.set(p, x, y, color)
+      pixels: Matrix.set(p, y, x, color)
     }
+  end
+
+  def to_ppm(%Canvas{width: w, height: h, pixels: p}) do
+    """
+    P3
+    #{w} #{h}
+    255
+    #{pixels_to_ppm(p)}
+    """
+  end
+
+  defp pixels_to_ppm(pixels) do
+    pixels
+    |> Enum.map(&pixel_row_to_ppm/1)
+    |> Enum.join("\n")
+  end
+
+  defp pixel_row_to_ppm(row) do
+    row
+    |> Enum.map(&pixel_to_ppm/1)
+    |> Enum.join(" ")
+  end
+
+  defp pixel_to_ppm(%Color{r: r, g: g, b: b}) do
+    "#{color_to_ppm(r)} #{color_to_ppm(g)} #{color_to_ppm(b)}"
+  end
+
+  defp color_to_ppm(c) when c <= 0.0, do: 0
+  defp color_to_ppm(c) when c >= 1.0, do: 255
+  defp color_to_ppm(c) do
+    floor(c * 256.0)
   end
 end
--- test/Dl44/canvas_test.exs
+++ test/Dl44/canvas_test.exs
@@ -51,14 +51,41 @@ defmodule Dl44.CanvasTest do
       red = %Color{r: 1.0, g: 0.0, b: 0.0}
       blue = %Color{r: 0.0, g: 0.0, b: 1.0}
       pixels = [
-        [red, red, red],
-        [red, blue, red],
-        [red, red, red]
+        [red, red, red, red],
+        [red, blue, red, red],
+        [red, red, red, blue]
       ]
-      canvas = %Canvas{width: 3, height: 3, pixels: pixels}
+      canvas = %Canvas{width: 4, height: 3, pixels: pixels}
 
       assert Canvas.pixel_at(canvas, 0, 0) == red
       assert Canvas.pixel_at(canvas, 1, 1) == blue
+      assert Canvas.pixel_at(canvas, 2, 3) == blue
+    end
+  end
+
+  describe "Canvas.to_ppm" do
+    test "returns a valid PPM string" do
+      c1 = Color.new(1.5, 0, 0)
+      c2 = Color.new(0, 0.5, 0)
+      c3 = Color.new(-0.5, 0, 1)
+      ppm = """
+      P3
+      5 3
+      255
+      255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+      0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
+      0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+      """
+
+      output =
+        Canvas.new(5, 3)
+        |> Canvas.write_pixel(0, 0, c1)
+        |> Canvas.write_pixel(2, 1, c2)
+        |> Canvas.write_pixel(4, 2, c3)
+        |> Canvas.to_ppm
+
+      assert output == ppm
+      assert String.last(output) === "\n"
     end
   end
 end
