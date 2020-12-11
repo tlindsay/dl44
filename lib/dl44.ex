2aa3994a77cb69562d0a576f1b065de872d95980
Patrick Lindsay <patrick.lindsay@dockyard.com>
2020-12-11 11:45:18 -0600

    Implement basic simulator

--- lib/dl44.ex
+++ lib/dl44.ex
@@ -1,5 +1,16 @@
 defmodule Dl44 do
-  defdelegate add(a, b), to: Dl44.Math
-  defdelegate sub(a, b), to: Dl44.Math
-  defdelegate scale(a, b), to: Dl44.Math
+  alias Dl44.Point
+  alias Dl44.Vector
+  alias Dl44.Math
+
+  def tick(_, %{position: %Point{x: _, y: y, z: _}, velocity: %Vector{}}) when y <= 0, do: IO.puts("DONE")
+  def tick(%{gravity: %Vector{}, wind: %Vector{}} = env, %{position: %Point{}, velocity: %Vector{}} = proj) do
+    position = Math.add(proj[:position], proj[:velocity])
+    velocity = proj[:velocity]
+               |> Math.add(env[:gravity])
+               |> Math.add(env[:wind])
+
+    IO.puts("X: #{position.x}, Y: #{position.y}, Z: #{position.z}")
+    tick(env, %{position: position, velocity: velocity})
+  end
 end
