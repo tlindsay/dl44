606cd3551f90738006869e5a19f484cc505a9a3d
Patrick Lindsay <patrick.lindsay@dockyard.com>
2021-03-12 17:12:26 -0600

    Finish implementing Matrix operations

--- lib/Dl44/matrix.ex
+++ lib/Dl44/matrix.ex
@@ -2,6 +2,8 @@ defmodule Dl44.Matrix do
   require Integer
   alias Dl44.Matrix
 
+  # TODO Clean up this module's organization
+
   def new(input) when is_tuple(input) do
     Tuple.to_list(input)
     |> Enum.map(&List.wrap/1)
@@ -9,27 +11,43 @@ defmodule Dl44.Matrix do
   end
   def new(input), do: Matrex.new(input)
 
+  def round(m = %Matrex{}), do: Matrix.round(m, 5)
+  def round(m = %Matrex{}, precision) do
+    Matrex.apply(m, fn val -> Float.round(val, precision) end)
+  end
+
+  def loose_equal?(m1, m2) do
+    Matrix.round(m1) == Matrix.round(m2)
+  end
+
   def multiply(m = %Matrex{}, t) when is_tuple(t) do
     m
     |>Matrex.dot(Matrix.new(t))
     |> Matrex.to_list
     |> List.to_tuple
   end
-  def multiply(i1, i2), do: Matrex.multiply(i1, i2)
+  def multiply(i1, i2), do: Matrex.dot(i1, i2)
 
   def identity(input), do: Matrex.identity(input)
 
   def transpose(input), do: Matrex.transpose(input)
 
   def at(m, r, c), do: Matrex.at(m, r+1, c+1)
+  def set(m, r, c, val), do: Matrex.set(m, r+1, c+1, val)
 
-  # def inverse(m) do
+  def inverse(m) do
+    if !invertible?(m), do: raise ArgumentError, message: "given matrix is not invertible"
 
-  # end
+    d = Matrix.determinant(m)
+    {row, col} = Matrex.size(m)
 
-  # def cofactor do
-
-  # end
+    for i <- 0..row-1, j <- 0..col-1, reduce: Matrex.zeros(row, col) do
+      acc ->
+        c = Matrix.cofactor(m, i, j)
+        Matrix.set(acc, i, j, (c / d))
+      end
+      |> Matrix.transpose
+  end
 
   def submatrix(m, r, c) do
     m
@@ -40,14 +58,26 @@ defmodule Dl44.Matrix do
   end
 
   def determinant(m) do
-    if (Matrex.size(m) == {2, 2}) do
-      a = Matrix.at(m, 0, 0)
-      b = Matrix.at(m, 1, 0)
-      c = Matrix.at(m, 0, 1)
-      d = Matrix.at(m, 1, 1)
+    det(m, Matrex.size(m))
+  end
 
-      (a*d) - (b*c)
-    end
+  defp det(m, {2, 2}) do
+    a = Matrix.at(m, 0, 0)
+    b = Matrix.at(m, 1, 0)
+    c = Matrix.at(m, 0, 1)
+    d = Matrix.at(m, 1, 1)
+
+    (a*d) - (b*c)
+  end
+  defp det(m, _) do
+    m
+    |> Matrex.to_list_of_lists
+    |> hd
+    |> Enum.map_reduce(0, fn x, index ->
+      {(x * Matrix.cofactor(m, 0, index)), index+1}
+    end)
+    |> elem(0)
+    |> Enum.sum
   end
 
   def minor(m, row, col) do
@@ -59,5 +89,9 @@ defmodule Dl44.Matrix do
   def cofactor(m, row, col) when Integer.is_even(row+col), do: minor(m, row, col)
   def cofactor(m, row, col) do
     minor(m, row, col) * -1
+  end
+
+  def invertible?(m) do
+    Matrix.determinant(m) != 0
   end
 end
--- test/Dl44/matrix_test.exs
+++ test/Dl44/matrix_test.exs
@@ -4,18 +4,35 @@ defmodule Dl44.MatrixTest do
 
   alias Dl44.Matrix
 
-  # Methods
-  # [ ] create
-  # [ ] compare
-  # [-] multiply matrices
-  # [ ] multiply matrix by tuple
-  # [-] create identity matrix
-  # [-] transpose matrix
-  # [ ] invert matrix
-  # [ ] calculate determinant
-  # [-] calculate submatrix(matrix, row, column)
-  # [ ] calculate minor(matrix, row, column)
-  # [ ] calculate cofactor
+  describe "Matrix.loose_equal?" do
+    test "it returns true for two strictlye equal matrices" do
+      m1 = Matrix.new("1,2,3; 4,5,6; 7,8,9")
+      m2 = Matrix.new("1,2,3; 4,5,6; 7,8,9")
+
+      assert Matrix.loose_equal?(m1, m2) == true
+    end
+
+    test "it returns true for two loosely equal matrices" do
+      m1 = Matrix.new("1,2,3; 4,5,6; 7,8,9")
+      m2 = Matrix.new("1.00000000001,2,2.999999999999; 4,5,6; 7,8,9")
+
+      assert Matrix.loose_equal?(m1, m2) == true
+    end
+
+    test "it returns false for two unequal matrices" do
+      m1 = Matrix.new("1,1,1; 2,2,2; 3,3,3")
+      m2 = Matrix.new("4,4,4; 5,5,5; 6,6,6")
+
+      assert Matrix.loose_equal?(m1, m2) == false
+    end
+
+    test "it returns false for two almost equal matrices" do
+      m1 = Matrix.new("1,1,1; 2,2,2; 3,3,3")
+      m2 = Matrix.new("1.1,1,1; 2,2,2; 3,3,3")
+
+      assert Matrix.loose_equal?(m1, m2) == false
+    end
+  end
 
   describe "Matrix.multiply" do
     test "can multiply by a tuple" do
@@ -29,13 +46,83 @@ defmodule Dl44.MatrixTest do
   end
 
   describe "Matrix.inverse" do
+    test "can calculate the inverse of a matrix" do
+      m = Matrix.new("-5,2,6,-8; 1,-5,1,8; 7,7,-6,-7; 1,-3,7,4")
+      inv = Matrix.new([
+        [ 0.21805,  0.45113,  0.24060, -0.04511],
+        [-0.80827, -1.45677, -0.44361,  0.52068],
+        [-0.07895, -0.22368, -0.05263,  0.19737],
+        [-0.52256, -0.81391, -0.30075,  0.30639]
+      ])
 
+      assert Matrix.determinant(m) == 532
+      assert Matrix.cofactor(m, 2, 3) == -160
+      assert m
+             |> Matrix.inverse
+             |> Matrix.loose_equal?(inv)
+    end
+
+    test "can calculate the inverse of a second matrix" do
+      m = Matrix.new("8,-5,9,2; 7,5,6,1; -6,0,9,6; -3,0,-9,-4")
+      inv = Matrix.new([
+        [ -0.15385, -0.15385, -0.28205, -0.53846 ],
+        [ -0.07692,  0.12308,  0.02564,  0.03077 ],
+        [  0.35897,  0.35897,  0.43590,  0.92308 ],
+        [ -0.69231, -0.69231, -0.76923, -1.92308 ]
+      ])
+
+      assert m
+             |> Matrix.inverse
+             |> Matrix.loose_equal?(inv)
+    end
+
+    test "can calculate the inverse of a third matrix" do
+      m = Matrix.new("9,3,0,9; -5,-2,-6,-3; -4,9,6,4; -7,6,6,2")
+      inv = Matrix.new([
+        [ -0.04074, -0.07778,  0.14444, -0.22222 ],
+        [ -0.07778,  0.03333,  0.36667, -0.33333 ],
+        [ -0.02901, -0.14630, -0.10926,  0.12963 ],
+        [  0.17778,  0.06667, -0.26667,  0.33333 ]
+      ])
+
+      assert m
+             |> Matrix.inverse
+             |> Matrix.loose_equal?(inv)
+    end
+
+    test "multiplying a matrix by its inverse" do
+      m1 = Matrix.new("3,-9,7,3; 3,-8,2,-9; -4,4,4,1; -6,5,-1,1")
+      m2 = Matrix.new("8,2,2,2; 3,-1,7,0; 7,0,5,4; 6,-2,0,5")
+      prod = Matrix.multiply(m1, m2)
+      inv = Matrix.inverse(m2)
+
+      assert Matrix.multiply(prod, inv) |> Matrix.loose_equal?(m1)
+    end
   end
 
   describe "Matrix.determinant" do
     test "can calculate determinant of 2x2 matrix" do
       m = Matrix.new("1,5;-3,2")
       assert Matrix.determinant(m) == 17
+    end
+
+    test "it can calculate determinant of a 3x3 matrix" do
+      m = Matrix.new("1,2,6; -5,8,-4; 2,6,4")
+      assert Matrix.cofactor(m, 0, 0) == 56
+      assert Matrix.cofactor(m, 0, 1) == 12
+      assert Matrix.cofactor(m, 0, 2) == -46
+      assert Matrix.determinant(m) == -196
+    end
+  end
+
+  describe "Matrix.invertible?" do
+    test "it can detect invertibility" do
+      m = Matrix.new("6,4,4,4; 5,5,7,6; 4,-9,3,-7; 9,1,7,6")
+      assert Matrix.invertible?(m) == true
+
+      m2 = Matrix.new("-4,2,-2,-3; 9,6,2,6; 0,-5,1,-5; 0,0,0,0")
+      assert Matrix.determinant(m2) == 0
+      assert Matrix.invertible?(m2) == false
     end
   end
 
