be331f86c90f214b02993d91ab153ae93699dc90
Patrick Lindsay <patrick.lindsay@dockyard.com>
2021-03-12 11:31:20 -0600

    Implement more matrix operations

--- lib/Dl44/matrix.ex
+++ lib/Dl44/matrix.ex
@@ -1,16 +1,63 @@
 defmodule Dl44.Matrix do
+  require Integer
   alias Dl44.Matrix
 
+  def new(input) when is_tuple(input) do
+    Tuple.to_list(input)
+    |> Enum.map(&List.wrap/1)
+    |> Matrix.new
+  end
   def new(input), do: Matrex.new(input)
 
   def multiply(m = %Matrex{}, t) when is_tuple(t) do
-    tup = Tuple.to_list(t)
-          |> Enum.map(&List.wrap/1)
-          |> Matrix.new
-
-    Matrex.dot(m, tup)
+    m
+    |>Matrex.dot(Matrix.new(t))
     |> Matrex.to_list
     |> List.to_tuple
   end
   def multiply(i1, i2), do: Matrex.multiply(i1, i2)
+
+  def identity(input), do: Matrex.identity(input)
+
+  def transpose(input), do: Matrex.transpose(input)
+
+  def at(m, r, c), do: Matrex.at(m, r+1, c+1)
+
+  # def inverse(m) do
+
+  # end
+
+  # def cofactor do
+
+  # end
+
+  def submatrix(m, r, c) do
+    m
+    |> Matrex.to_list_of_lists
+    |> Enum.map(fn row -> List.delete_at(row, c) end)
+    |> List.delete_at(r)
+    |> Matrix.new
+  end
+
+  def determinant(m) do
+    if (Matrex.size(m) == {2, 2}) do
+      a = Matrix.at(m, 0, 0)
+      b = Matrix.at(m, 1, 0)
+      c = Matrix.at(m, 0, 1)
+      d = Matrix.at(m, 1, 1)
+
+      (a*d) - (b*c)
+    end
+  end
+
+  def minor(m, row, col) do
+    m
+    |> Matrix.submatrix(row, col)
+    |> Matrix.determinant
+  end
+
+  def cofactor(m, row, col) when Integer.is_even(row+col), do: minor(m, row, col)
+  def cofactor(m, row, col) do
+    minor(m, row, col) * -1
+  end
 end
--- test/Dl44/matrix_test.exs
+++ test/Dl44/matrix_test.exs
@@ -4,6 +4,19 @@ defmodule Dl44.MatrixTest do
 
   alias Dl44.Matrix
 
+  # Methods
+  # [ ] create
+  # [ ] compare
+  # [-] multiply matrices
+  # [ ] multiply matrix by tuple
+  # [-] create identity matrix
+  # [-] transpose matrix
+  # [ ] invert matrix
+  # [ ] calculate determinant
+  # [-] calculate submatrix(matrix, row, column)
+  # [ ] calculate minor(matrix, row, column)
+  # [ ] calculate cofactor
+
   describe "Matrix.multiply" do
     test "can multiply by a tuple" do
       m = Matrix.new([[1, 2, 3, 4],
@@ -20,14 +33,34 @@ defmodule Dl44.MatrixTest do
   end
 
   describe "Matrix.determinant" do
-
+    test "can calculate determinant of 2x2 matrix" do
+      m = Matrix.new("1,5;-3,2")
+      assert Matrix.determinant(m) == 17
+    end
   end
 
   describe "Matrix.minor" do
+    test "can calculate the minor of a matrix" do
+      m = Matrix.new("3,5,0; 2,-1,-7; 6,-1,5")
+      assert Matrix.minor(m, 1, 0) == 25
+    end
+  end
 
+  describe "Matrix.submatrix" do
+    test "it drops the specified row/col" do
+      m = Matrix.new("3,5,0; 2,-1,-7; 6,-1,5")
+      assert Matrix.submatrix(m, 1, 1) == Matrix.new("3,0; 6,5")
+    end
   end
 
   describe "Matrix.cofactor" do
+    test "it calculates cofactors" do
+      m = Matrix.new("3,5,0; 2,-1,-7; 6,-1,5")
 
+      assert Matrix.minor(m, 0, 0) == -12
+      assert Matrix.cofactor(m, 0, 0) == -12
+      assert Matrix.minor(m, 1, 0) == 25
+      assert Matrix.cofactor(m, 1, 0) == -25
+    end
   end
 end
