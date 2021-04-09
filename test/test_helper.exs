b0fbfda700438e22846e1a5c15c27f69130f3636
Patrick Lindsay <patrick.lindsay@dockyard.com>
2021-04-09 12:29:03 -0500

    Add pipe-able test helpers

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,21 @@
 ExUnit.start()
+
+# Pipe-able assertions
+# Stolen from: https://dev.to/wulymammoth/using-pipes-in-your-test-assertions-43dk
+defmodule TestHelper do
+  defmacro __using__(_opts) do
+    quote do
+      import ExUnit.Assertions, only: [assert: 1]
+
+      def is(result, expectation) do
+        assert result == expectation
+        result
+      end
+
+      def has(result, assertion) when is_function(assertion) do
+        assert assertion.(result) == true
+        result
+      end
+    end
+  end
+end
